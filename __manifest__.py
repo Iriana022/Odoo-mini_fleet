e79cd5593cba6ebfc4ad7423066d969e3465e0cb
Iriana R. <dev32.irin@gmail.com>
2026-07-21 11:47:44 +0300

    update: add some value to manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,9 @@
 {
     'name': 'Mini fleet',
-    'version': '1.0',
+    'version': '16.0.1.0.0',
+    'category' : 'Fleet Management',
+    'author': 'Iriana R / Materauto',
+    'license': 'LGPL-3',
     'summary': 'Pour gérer un registre de véhicules d\'entreprise. Chaque véhicule est identifié par son numéro de châssis, notion directement transposable au concept de numéro de lot dans Odoo Stock.',
     'depends': ['base', 'contacts'],
     'data': [
